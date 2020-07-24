a5e1303928792c1a40dda789afb14af2d3a09202
Brad <bradsiefert@icloud.com>
2020-07-24 15:28:22 -0500

    Styling the about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,20 +1,106 @@
 <template>
   <Layout>
-    <section id="container-centre" class="column centre flex-1">
-      <h1 class="page-title">About</h1>
-      <div class="content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolor ipsum voluptatum dicta eveniet commodi molestiae! Suscipit, magnam cumque, illum ab amet ipsum deserunt distinctio est dolore dolor ad ipsa cupiditate minus soluta, possimus accusamus.</p>
-        <p>Ipsa facilis sint, beatae aperiam enim expedita voluptas dolores suscipit reiciendis similique alias, odio repellendus placeat asperiores error laboriosam eligendi accusantium in eos! Dolores labore excepturi doloremque. Rem ipsa quae tempore dignissimos doloribus recusandae quam quibusdam mollitia ex voluptas, repellat dolor et beatae sint nesciunt ad cupiditate cum debitis dolorum earum? Error adipisci optio in ea accusamus incidunt eligendi consequatur, quod impedit dolorem atque quaerat?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolor ipsum voluptatum dicta eveniet commodi molestiae! Suscipit, magnam cumque, illum ab amet ipsum deserunt distinctio est dolore dolor ad ipsa cupiditate minus soluta, possimus accusamus. Ipsa facilis sint, beatae aperiam enim expedita voluptas dolores suscipit reiciendis similique alias, odio repellendus placeat asperiores error laboriosam eligendi accusantium in eos! Dolores labore excepturi doloremque. Rem ipsa quae tempore dignissimos doloribus recusandae quam quibusdam mollitia ex voluptas, repellat dolor et beatae sint nesciunt ad cupiditate cum debitis dolorum earum? Error adipisci optio in ea accusamus incidunt eligendi consequatur, quod impedit dolorem atque quaerat?</p>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="breadcrumb">
+            <div class="breadcrumb-item"><a title="Link back to home" href="/">Home</a></div>
+            <div class="breadcrumb-item">{{ $metaInfo.title }}</div>
+          </div>
+        </div>
       </div>
-    </section>
+
+      <div class="row about">
+        <div class="col-md-7 col-lg-8">
+          <article>
+            <h2 class="h1">I promise this won’t be in the third person.</h2>
+            <p>
+              Hi, I&#8217;m Brad, and I&#8217;m a UI/UX designer experienced in designing web apps, mobile apps, 
+              and front-end web development. I live in the 
+              <a target="_blank" title="Link to East Garfield Park's Wikipedia" href="https://en.wikipedia.org/wiki/East_Garfield_Park,_Chicago">
+              East Garfield Park</a> neighborhood of Chicago with my wife Nicole and my daughter Lydia.
+            </p>
+
+            <p>
+              I value elegance, clarity, and efficiency in design. I always strive to consider 
+              the user&#8217;s needs and how to best serve them. I love working with people who inspire me to learn and 
+              who are serious about creating beautiful and meaningful work.
+            </p>
+
+            <p>
+              Since 2018, I&#8217;ve been on the board of a homelessness non-profit, 
+              <a target="_blank" title="Link to Open Arms Ministry" href="https://openarmsministry.org">Open Arms</a>, 
+              focused on improving our internal systems and IT. It&#8217;s been a great opportunity to learn how to 
+              blend code and nocode solutions to create more effective operations.
+            </p>
+
+            <p>
+              Each year I take around 50,000 photos. I want to learn two languages in the next couple of years: Spanish 
+              and JavaScript. I drink <em>way</em> too much Diet Coke—but I’m working on it.
+            </p>
+
+            <p>
+              <small>
+                Currently I&#8217;m obsessed with:<br/>
+                <a target="_blank" title="Link to Design Systems Repo" href="https://designsystemsrepo.com">
+                  Design Systems
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to Apple's Shortcuts app" href="https://apps.apple.com/us/app/shortcuts/id1462947752">
+                  Siri Shortcuts
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to iPadOS" href="https://www.apple.com/ipados/">
+                  iPadOS
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to Airtable" href="https://airtable.com">
+                  AirTable
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to Makerpad" href="https://www.makerpad.co">
+                  Nocode tools
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to The Ringer" href="https://www.theringer.com">
+                  The Ringer
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to the Relay FM network" href="https://www.relay.fm">
+                  Relay FM
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to the Do By Friday podcast" href="http://dobyfriday.com">
+                  Do By Friday
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to Polaroid Originals instant film" href="https://us.polaroidoriginals.com">
+                  Instant film
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to Chelsea FC soccer team" href="https://www.instagram.com/chelseafc/">
+                  Chelsea FC
+                </a>&nbsp;&nbsp; 
+                <a target="_blank" title="Link to The Ringer" href="https://podcasts.apple.com/us/podcast/the-ringer-nba-show/id1109271715">
+                  NBA podcasts
+                </a>&nbsp;&nbsp;
+              </small>
+            </p>
+
+            <a target="_blank" class="btn btn-primary mb-32" href="files/Brad-Siefert-Resume.pdf">
+              Download Resume
+            </a>
+          </article>
+        </div>
+
+        <div class="col-md-5 col-lg-4">
+          <img class="img-fluid" src="images/photo-bradsiefert.jpg" alt="Photo of Brad Siefert">
+        </div>
+
+      </div>
+    </div>
+
   </Layout>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: "About"
+  metaInfo() {
+    return {
+      title: 'About'
+    };
   }
 };
 </script>
