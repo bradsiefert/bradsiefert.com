ac50e0ba3783ac5eb422f99545515497faf6fb54
Brad <bradsiefert@icloud.com>
2020-07-28 17:49:12 -0500

    Added 404

---
+++ src/pages/404.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <g-image 
+          class="mb-16" 
+          src="~/assets/images/clueless-my-bad.gif" 
+          alt="Gif from the movie Clueless of Cher saying My bad!" 
+          />
+          <h1>404 Error: Page Not Found</h1>
+          <p>
+            I'm sorry about this. If you'd let me know about this on 
+            <a target="_blank" href="https://twitter.com/bksiefert">Twitter</a>, I'd appreciate it. <br>
+            Have a good day!
+          </p>
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '404'
+  }
+}
+</script>
+
+<style>
+</style>
