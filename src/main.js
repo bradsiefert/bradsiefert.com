ee42daf498aede388cecf31fb9a5bbeb870f7f31
Brad <bradsiefert@icloud.com>
2020-08-03 20:34:56 -0500

    Testing to try and fix FA Safari bug

--- src/main.js
+++ src/main.js
@@ -10,15 +10,15 @@ import moment from 'moment';
 // Adding FontAwesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { faGithub, faTwitter, faDribbble, faInstagram, faLinkedin, faFlickr, faProductHunt, faMedium } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faTwitter, faDribbble, faInstagram, faLinkedin, faProductHunt, faMedium } from '@fortawesome/free-brands-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false;
-library.add(faGithub, faTwitter, faDribbble, faInstagram, faLinkedin, faFlickr, faProductHunt, faMedium)
+config.autoAddCss = true;
+library.add(faGithub, faTwitter, faDribbble, faInstagram, faLinkedin, faProductHunt, faMedium)
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
-  
+
   // Import FontAwesome
   Vue.component('font-awesome', FontAwesomeIcon)
 
