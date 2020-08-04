ca072a2a196c6c9df2cc23b14937eb0fc975998b
Brad <bradsiefert@icloud.com>
2020-08-04 11:20:07 -0500

    adding a space

--- src/layouts/FrontPage.vue
+++ src/layouts/FrontPage.vue
@@ -18,7 +18,7 @@
           </div>
 
           <h1 class="display-1">
-            Hi, 👋🏻 I’m Brad and I’m a UI/UX Designer from Chicago, IL.<br /><br />
+            Hi, 👋🏻 &nbsp;I’m Brad and I’m a UI/UX Designer from Chicago, IL.<br /><br />
 
             I love code and #nocode.<br />
             I’m a Siri Shortcuts enthusiast.<br /> 
@@ -163,14 +163,14 @@ export default {
         { property: "og:title", content: "Brad Siefert / Designer & Photographer" },
         { property: "og:description", content: "Personal design portfolio and blog of Chicago based UI/UX Designer Brad Siefert." },
         { property: "og:url", content: "https://brad.si/efert" },
-        { property: "og:image", content: "https://www.brad.si/efert/assets/static/src/assets/images/sharing.jpg" },
+        { property: "og:image", content: "https://www.bradsiefert.com/assets/static/src/assets/images/sharing.jpg" },
 
         { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:title", content: "Brad Siefert / Designer & Photographer" },
         { name: "twitter:description", content: "Personal design portfolio and blog of Chicago based UI/UX Designer Brad Siefert." },
         { name: "twitter:site", content: "@bksiefert" },
         { name: "twitter:creator", content: "@bksiefert" },
-        { name: "twitter:image", content: "https://www.brad.si/efert/assets/static/src/assets/images/sharing.jpg" }
+        { name: "twitter:image", content: "https://www.bradsiefert.com/assets/static/src/assets/images/sharing.jpg" }
       ]
     };
   },
