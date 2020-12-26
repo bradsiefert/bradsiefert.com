c75455539cc019df8f0435fe8489d4e6a5b2e683
Brad Siefert <bksiefert@Brads-M1-MacBook-Pro.local>
2020-12-26 15:00:01 -0600

    Playing with building json on build

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,9 +1,48 @@
 // Server API makes it possible to hook into various parts of Gridsome
 // on server-side and add custom data to the GraphQL data layer.
-// Learn more: https://gridsome.org/docs/server-api/
+// Learn more: https://gridsome.org/docs/server-api
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+
+// const fs = require('fs');
+// const path = require('path');
+// const pick = require('lodash.pick');
+// 
+// module.exports = function (api, options) {
+//   api.loadSource(store => {
+//     // Use the Data store API here: https://gridsome.org/docs/data-store-api
+//   })
+// 
+//   api.beforeBuild(({ config, store }) => {
+// 
+//     // Generate an index file for Fuse to search Posts
+//     const { collection } = store.getContentType('Blog');
+// 
+//     const Blog = collection.data.map(post => {
+//       return pick(post, ['title', 'path', 'description']);
+//     });
+// 
+//     const output = {
+//       dir: './static',
+//       name: 'searchresults-blog.json',
+//       ...options.output
+//     }
+// 
+//     const outputPath = path.resolve(process.cwd(), output.dir)
+//     const outputPathExists = fs.existsSync(outputPath)
+//     const fileName = output.name.endsWith('.json')
+//       ? output.name
+//       : `${output.name}.json`
+// 
+//     if (outputPathExists) {
+//       fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(Blog))
+//     } else {
+//       fs.mkdirSync(outputPath)
+//       fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(Blog))
+//     }
+//   })
+// }
 
 module.exports = function (api) {
   api.loadSource(({ addCollection }) => {
