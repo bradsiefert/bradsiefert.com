611cbf6048d5054d3097bffd48b66651a5fcee84
Brad Siefert <bksiefert@Brads-M1-MacBook-Pro.local>
2020-12-26 14:59:46 -0600

    Creating the search function

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,31 +1,200 @@
 <template>
   <div>
+    <search-focus @keyup="focusSearch"></search-focus>
     <div class="search-bar">
-      <input type="text" class="form-control" id="fuse-search" placeholder="🔍 Search...">
+      <input 
+        type="text" 
+        class="form-control" 
+        id="fuse-search" 
+        placeholder="🔍 Search..."
+        v-model="query"
+        @blur="searchResultsVisible = false"
+        @focus="searchResultsVisible = true"
+        @keydown.esc="searchResultsVisible = false"
+        @input="searchResultsVisible = true"
+        @keydown="performSearch"
+        ref="search"
+      >
+      <div 
+        class="close"
+        v-if="query.length > 0"
+        @click="reset"
+      >
+        &times;
+      </div>
     </div>
     
-    <!-- <div class="search-bar-results">
-      <div class="results-wrap">
-        <div class="results-head"><a href="">Running Untrusted Siri Shortcuts</a></div>
-        <p class="results-description">I want to download this Siri Shortcut but I can't add it because it says it's "Untrusted" and I don't know how to make it run. Help!</p>  
+    <div v-if="query.length > 1 && searchResultsVisible" class="search-bar-results">
+      <div class="results-wrap" v-for="(post, index) in searchResults" :key="index">
+        <a 
+          :href="post.item.path" 
+          @mousedown.prevent="searchResultsVisible = true"
+          :class="{ 'results-active': index === highlightedIndex }"
+        >
+          <div class="results-head">
+            {{ post.item.title }}
+          </div>
+          <p class="results-description">
+            {{ post.item.description }}
+          </p>
+        </a>
       </div>
-      <div class="results-wrap">
-        <div class="results-head"><a href="">It's Time for iOS Developers to Embrace the FAB</a></div>
-        <p class="results-description">I’ve always found Apple’s iOS User Interface ethos a bit odd. When you read Apple’s iOS Human Interface Guidelines, it feels sparse. The HIG is supposed to be</p>
+      <div v-if="query.length > 1 && searchResults.length === 0"  class="px-3 py-2">
+        <div class="results-description mb-0">No results for <strong>"{{ query }}"</strong>.</div>
       </div>
-      <div class="results-wrap">
-        <div class="results-head"><a href="">Connect Guests to Your Wifi with a QR Code</a></div>
-        <p class="results-description">I love it when you find a small way to use technology to make something mundane a bit easier. Inputting a wifi password is a tiny annoyance that we’ve all had to deal now for quite a while...</p>
-      </div>
-      <div class="px-3 py-2">
-        <div class="results-description mb-0">No results for: <strong>"zander"</strong></div>
-      </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
+import SearchFocus from '@/components/SearchFocus.vue'
+import axios from 'axios'
+
 export default {
-  name: 'SearchBar'
+  name: 'SearchBar', 
+  components: {
+    SearchFocus
+  },
+  data() {
+    return {
+      query: '',
+      searchResultsVisible: false,
+      posts: [],
+      searchResults: [],
+      highlightedIndex: 0,
+      options: {
+        shouldSort: true,
+        includeMatches: true,
+        threshold: 0.5,
+        location: 0,
+        distance: 500,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: ['title', 'description']
+      }
+    }
+  },
+  created() {
+    axios.get('/searchresults.json')
+      .then(response => {
+        this.posts = response.data
+      })
+  },
+  methods: {
+    reset() {
+      this.query = ''
+    },
+    performSearch() {
+      this.$search(this.query, this.posts, this.options)
+        .then(results => {
+          this.searchResults = results
+        })
+    },
+    focusSearch(e) {
+      if (e.key === '/') {
+        this.$refs.search.focus()
+      }
+    },
+  }
 }
 </script>
+
+<style lang="scss">
+@import "@/assets/scss/variables.scss";
+
+.search-bar {
+  position: fixed;
+  top: 32px;
+  right: 32px;
+}
+
+@media (max-width: 1024px) {
+  .search-bar {
+    display: none;
+  }
+}
+
+.search-bar .form-control,
+.search-bar .form-control:focus,
+.search-bar-results {
+  width: 160px;
+  border-radius: 1rem !important;
+  height: 32px;
+  font-size: $font-size-300;
+  color: $black;
+  font-weight: 500;
+  opacity: 1;
+  z-index: 99;
+}
+
+.search-bar-results {
+  z-index: 100;
+  height: auto;
+  min-width: 320px;
+  position: fixed;
+  top: 68px;
+  right: 32px;
+  // max-height: 288px;
+  // overflow-y: auto;
+}
+
+.search-bar-results a {
+  color: $black;
+}
+
+.results-wrap {
+  padding: 1rem;
+  border-bottom: 1px solid $gray-200;
+}
+
+.search-bar-results a > .results-wrap:hover {
+  text-decoration: none;
+}
+
+.results-wrap:last-of-type {
+  border-bottom: none;
+}
+
+.results-wrap:hover, .results-active {
+  background-color: #d4f4ff;
+}
+
+.results-wrap a:hover {
+  text-decoration: none;
+}
+
+.results-wrap:hover:last-of-type {
+  border-bottom-left-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+}
+
+.results-wrap:hover:first-of-type {
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+}
+
+.results-head {
+  font-size: $font-size-500;
+  font-family: "Tiempos Text";
+  font-weight: 700;
+  margin-bottom: 0;
+  line-height: 1.25;
+}
+
+.results-description {
+  font-size: $font-size-100;
+  margin-bottom: 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.search-bar .close {
+  position: absolute;
+  top: 0;
+  right: 12px;
+  font-size: 1.25rem;
+  color: $gray-500;
+  cursor: pointer;
+}
+</style>
