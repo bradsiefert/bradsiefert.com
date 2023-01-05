5c5ab007c7d900e6ff41b1b4f638efb6b89402b0
Brad Siefert <bradsiefert@icloud.com>
2023-01-05 16:18:11 -0600

    Fixing small head and css issues (font weight, #app, bootstrap)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,21 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Brad Siefert / Designer & Photographer',
+      meta: [
+        { name: 'description', content: 'Personal design portfolio and blog of Chicago based Design Engineer Brad Siefert.' }
+      ],
+    }
+  },
   modules: ['@nuxt/content'],
   content: {
     documentDriven: true
-  }
+  },
+  css: [
+    '@/assets/scss/styles.scss',
+    // Needed to install sass and bootstrap to make this work.
+  ]
 })
