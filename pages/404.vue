3a289e86db94dbafaef1c120dcf7db1e8eb062ff
Brad Siefert <bradsiefert@icloud.com>
2023-01-08 21:03:56 -0600

    Making pages one by one

---
+++ pages/404.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <div class="container skinny-contain">
+      <div class="row">
+        <div class="col">
+          <div class="breadcrumb">
+            <div class="breadcrumb-item"><a title="Link back to home" href="/">Home</a></div>
+            <div class="breadcrumb-item">404 Page</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <img
+            class="img-fluid mb-3"
+            width="320"
+            src="@/assets/images/clueless-my-bad.gif"
+            alt="Gif from the movie Clueless of Cher saying My bad!"
+          />
+          <h1>404 Error: Page Not Found</h1>
+          <p>
+            I'm sorry about this. If you'd let me know about this on
+            <a target="_blank" href="https://twitter.com/bksiefert">Twitter</a>, I'd appreciate it. <br>
+            Have a good day!
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
