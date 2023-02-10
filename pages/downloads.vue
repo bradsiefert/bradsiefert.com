3b810581ba74a7585949729dd0295cd6d53b0685
Brad Siefert <bradsiefert@icloud.com>
2023-02-10 15:40:28 -0600

    Adding the Super Bowl Game to the Downloads page

--- pages/downloads.vue
+++ pages/downloads.vue
@@ -9,9 +9,35 @@
       <div class="row g-3">
         <div class="col-xl-6">
           <div class="box">
-            <div class="d-flex flex-column" style="min-height: 288px;">
+            <div class="d-flex flex-column" style="min-height: 256px;">
+              <p class="blog-details">2023-02-10</p>
+              <div class="mt-auto"><h2 class="h4 mb-2">Super Bowl 57 Party Game <span class="badge text-bg-primary">New</span></h2></div>
+              <p class="font-size-300">
+                I have an annual tradition of having friends over to watch the Super Bowl. Each year I make this PDF party game and it's always a lot of fun. I'm proud of the game because casual football fans have a decent shot of winning even against a big football fan. Check it out and please use it at your own party!
+              </p>
+
+              <p class="font-size-100 font-monospace"><strong>FILE TYPE:</strong> PDF and Pages</p>
+
+              <div class="mt-auto">
+                <div class="d-grid gap-2 d-flex">
+                  <a target="_blank" class="btn btn-outline-dark btn-sm my-1 me-sm-2" href="files/Super-Bowl-57-Party-Game.pdf">
+                    Download PDF &nbsp;⬇️
+                  </a>
+
+                  <a target="_blank" class="btn btn-outline-dark my-1 btn-sm" href="files/Super-Bowl-57-Party-Game.pages">
+                    Download Pages &nbsp;⬇️
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-6">
+          <div class="box">
+            <div class="d-flex flex-column" style="min-height: 256px;">
               <p class="blog-details">2020-08-11</p>
-              <div class="mt-auto"><h2 class="mb-2">Gridsome Neumorphic Starter</h2></div>
+              <div class="mt-auto"><h2 class="h4 mb-2">Gridsome Neumorphic Starter</h2></div>
               <p class="font-size-300">
                 Are you enjoying the website you are currently viewing? Would you like to make this site your own? Well I made it into a starter for the static site generator <a target="_new" href="https://gridsome.org/">Gridsome</a>, which is what powers the site. Instructions to install are at my Github.
               </p>
@@ -31,9 +57,9 @@
 
         <div class="col-xl-6">
           <div class="box">
-            <div class="d-flex flex-column" style="min-height: 288px;">
+            <div class="d-flex flex-column" style="min-height: 256px;">
               <p class="blog-details">2020-06-21</p>
-              <div class="mt-auto"><h2 class="mb-2">Sketch Resume Template</h2></div>
+              <div class="mt-auto"><h2 class="h4 mb-2">Sketch Resume Template</h2></div>
               <p class="font-size-300">
                 Job hunting can be such a terrible and soul crushing pursuit. I hope that his resume template helps make a few things a bit easier. I'm really proud of this design. Best to you in your search.
               </p>
@@ -57,9 +83,9 @@
 
         <div class="col-xl-6">
           <div class="box">
-            <div class="d-flex flex-column" style="min-height: 288px;">
+            <div class="d-flex flex-column" style="min-height: 256px;">
               <p class="blog-details">2020-02-10</p>
-              <div class="mt-auto"><h2 class="mb-2">Enneagram Triads & Stances</h2></div>
+              <div class="mt-auto"><h2 class="h4 mb-2">Enneagram Triads & Stances</h2></div>
               <p class="font-size-300">
                 The Enneagram has been an incredibly helpful tool for spiritual growth in my life. My therapist had a hand drawn version of this drawing and I decided to design a digital version. There are a lot of variations of language in the Enneagram, but this language is what has spoken most clearly to me.
               </p>
@@ -83,9 +109,9 @@
 
         <div class="col-xl-6">
           <div class="box">
-            <div class="d-flex flex-column" style="min-height: 288px;">
+            <div class="d-flex flex-column" style="min-height: 256px;">
               <p class="blog-details">2019-09-15</p>
-              <div class="mt-auto"><h2 class="mb-2">10 Years of Photos Book</h2></div>
+              <div class="mt-auto"><h2 class="h4 mb-2">10 Years of Photos Book</h2></div>
               <p class="font-size-300">
                 2017 marked 10 years of taking photographs and I wanted to mark that time with a book. I laid out and printed 25 large photo books (which were not cheap!) and distributed them among friends and family. This is the print PDF of that book.
               </p>
