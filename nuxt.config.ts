9c75cd3d2771fcaa634b2f44f5e2b4d50fc3c181
Brad Siefert <bradsiefert@icloud.com>
2023-02-22 10:18:35 -0600

    Remote extra paren

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,12 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1"',
+      viewport: 'width=device-width, initial-scale=1',
       title: 'Brad Siefert / UX Designer & Photographer',
       meta: [
         { name: 'description', content: 'Design portfolio and personal blog of Chicago-based Design Technologist and photographer, Brad Siefert.' },
         { property: "og:image", content: 'https://bradsiefert.com/sharing.jpg' },
+        // Fix the issues with https://opengraph.dev / https://ogp.me
       ],
     }
   },
