e838ab8f19c72e57b472a5b7d33c4c764019db99
Brad Siefert <bradsiefert@users.noreply.github.com>
2025-10-03 16:47:46 -0500

    GovOS Case Study content v1

--- pages/case-studies/govos-design-system.vue
+++ pages/case-studies/govos-design-system.vue
@@ -8,8 +8,8 @@
             <NuxtLink to="/portfolio">Portfolio</NuxtLink> / 
             <span>GovOS Design System</span>
           </div>
-
           <div class="pt-md">
+            <div class="my-sm fs-lg-semibold text-uppercase">Case Study</div>
             <h1>GovOS Design System</h1>
             <p>In 2021, I joined GovOS as the company's first in-house designer. They'd pieced design together for years through contractors and had no centralized visual design or design system. I was tasked to build a complete design system from scratch. I build foundations like color scale and typescale, 30+ components, and managed building the system in React.</p>
 
@@ -25,7 +25,7 @@
 
     <div class="case-study govos">
       <div class="container wide-contain">
-        <img class="govos-components img-fluid mt-xl" src="@/assets/images/govos-design-system-casestudy-light.png" alt="GovOS Design System UI Components"/>
+        <img class="govos-components img-fluid mt-xl" src="@/assets/images/govos-design-system-casestudy-light.webp" alt="GovOS Design System UI Components"/>
         <figcaption class="mt-md mb-none">Example GovOS Design System components</figcaption>
 
         <img class="govos-patterns img-fluid mt-xl" src="@/assets/images/govos-design-system-casestudy-design-patterns-light.webp" alt="GovOS Design System UI Components"/>
@@ -54,15 +54,15 @@
 
           <hr>
 
-          <h2>Visual Design Rules</h2>
+          <h2>Visual Design Principles</h2>
           <p>Whenever I start a design, I am always on the lookout for how the visual design rules dictate to look and feel. Choosing a bold color palette or an extremely minimalist aesthetic looks great in Figma, but our users needed something more dependable and straightforward in the real world. When I began building the GovOS design system, I wanted the visual language to reflect <strong>clarity, trust, and accessibility.</strong> The audience for these products isn't creative professionals used to subtle visual nuance. It's users are citizens paying taxes, requesting records, and managing essential services. Every element needs to be <strong>obvious, direct, and legible.</strong></p>
 
-          <p>A few of these choices became guiding lights for the system:</p>
+          <p>These choices became guiding lights for the system:</p>
           <ul>
-            <li><strong>A basic and bright palette.</strong> My color palette isn't flashy, but it's reliable. I anchored it around my <code>color-blue-600</code> color for primary actions and chose neutrals that pair perfectly. The goal was to feel solid and professional and honestly at times, middle of the road. I never wanted it to feel overly branded or distracting.</li>
-            <li><strong>A single typeface.</strong> .....</li>
-            <li><strong>Almost no shadows.</strong> Instead of trendy shadows or heavy visual depth, I leaned into a flatter aesthetic. Cards and surfaces are defined with a light border (<code>color-neutral-300</code> in light mode) and were always set on <code>color-neutral-100</code> to have a subtle but clear contrast from the background. I wanted our UI to feel like a sheet of paper on a desk. This created a clean separation without any visual noise.</li>
-            <li><strong>High contrast.</strong> Accessibility was non-negotiable. Rather than meeting minimum contrast requirements, we pushed far beyond them. Subtle text styles or nearly indistinguishable shades were avoided. Our users needed clear differences in states, hierarchy, and affordances, so everything was designed for maximum clarity</li>
+            <li class="mb-lg"><strong>A basic and bright palette.</strong> My color palette isn't flashy, but it's reliable and predictable. I anchored it around my <code>color-blue-600</code> color for primary actions and chose neutrals that pair perfectly. The goal was to feel solid and professional and honestly at times, middle of the road. I never wanted it to feel overly branded or distracting. I often preached to hat our colors must follow the color roles <code>(primary, danger, warning, success, & highlight)</code> we setup to ensure consistency.</li>
+            <li class="mb-lg"><strong>A single typeface and a large type scale.</strong> I feel strongly that all typography should be tied to a type scale via design tokens. This ensures consistent line heights, letter spacing, and makes visual rhythm easier to achieve. The list of great UI typefaces is not very long and for this system I chose Source Sans 3 because it's previous use in federal web applications, clean aesthetic, and variable font weight to allow me to create a nice contrast between the three font weights.</li>
+            <li class="mb-lg"><strong>Clean borders and almost no shadows.</strong> Instead of trendy shadows or heavy visual depth, I leaned into a very flat aesthetic. Cards and surfaces are defined with a light border (<code>color-neutral-300</code> in light mode) and were always set on <code>color-neutral-100</code> to have a subtle but clear contrast from the background. I wanted our UI to feel like a sheet of paper on a desk. This created a clean separation without any visual noise.</li>
+            <li class="mb-2xl"><strong>High contrast and accessibile.</strong> GovOS's previous used overly subtle color opacities for their color palette. At times, they were almost indistinguishable from each other causing usability and accessibility issues. Accessibility had not been taken seriously in the past, but for me, it was non-negotiable. Rather than barely meeting minimum contrast requirements for text, I setup the system to well exceed them. Subtle text styles, tints, or shades were avoided. Our users needed clear differences in states, hierarchy, and affordances, so everything was designed for maximum clarity.</li>
           </ul>
           <p>
             <span class="wide-img">
@@ -98,15 +98,20 @@
                 <img src="/portfolio/case-study-govos-colors-darkmode.webp" alt="">
               </a>
             </span>
-            <!-- <figcaption>GovOS' Tax and Licensing product for states and counties built with the new design system</figcaption> -->
           </p>
           <hr>
 
-          <h2>Documenting for Design & Development</h2>
-
-          <hr>
+          <!-- <h2>Documenting for Design & Development</h2>
+          <p>I spend a huge amount of timing writing and creating design documentation for the system. I want the system to be incredibly easy to understand for both designers and developers. I think documentation is the differentiator between a system that is used and a system that is ignored. I occasionally contributed to the development documentation, but I wrote and created all of the design documentation and assets in Figma.</p>
+            
+          <p>Content writing seems like such a straightforward task, but anyone who has tried it knows will tell you it's actually incredibly difficult. It requires a deep understanding of design principles and engineering best practices and the ability to write in a way that is both precise and concise. It also requires a lot of time and effort to get right.</p>
+          <hr> -->
 
           <h2>In Practice</h2>
+          <p>Once the system foundations were solid and core components were created, the next step was collaborating with other UX designers to create mockups and user flows. Designers used the system to create a redesigned tax portal for State of Colorado and Alaska, updated our public search portal, and created platform components to be shared across all applications.</p>
+          
+          <p>Collaborating with other designers is important to me and comes naturally for me. Especially during the design process for the tax portal redesigns, I collaborated closely with designers to adapt components to their needs and participated in design critiques and reviews to make sure they knew my intentions when designing the components and patterns. It was the first stress test for using the system in such a large scale and on such a short timeline. It performed extremely well and allowed me to focus on the larger goals of maturing the system while still maintaining consistency across the product suite.</p>
+          
           <p>
             <span class="ultrawide-img">
               <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-apps-cdor.webp" class="lightbox-trigger">
