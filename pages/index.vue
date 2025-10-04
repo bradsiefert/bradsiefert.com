1d2243390f1fbbc624774691964b84133b988451
Brad Siefert <bradsiefert@users.noreply.github.com>
2025-10-04 09:49:59 -0500

    Light updates to copy and css

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container skinny-contain hero">
     <h1 class="text-center mb-xl">Hey 👋, I’m Brad.</h1>
     <div class="subtitle fs-3xl-regular">
-      I’m a <strong>Product Designer</strong> and <strong>Design Technologist</strong> experienced building apps and design systems. I’m fluent in <strong>Figma</strong> & <strong>code</strong>. I love creating UIs that are elegant, impactful, and accessible.
+      I'm a <strong>Product Designer</strong> and <strong>Design Technologist</strong> experienced in building apps and design systems. I'm fluent in <strong>Figma</strong> & <strong>code</strong>. I love creating UIs that are elegant, impactful, and accessible.
     </div>
   </div>
   <portfolio />
