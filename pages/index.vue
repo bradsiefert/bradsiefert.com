3ac5245e6eb3bd391d3dee562fc27bd5eb790ea8
Brad Siefert <bradsiefert@icloud.com>
2026-02-06 12:25:49 -0600

    Adding hero highlighter and animated wave

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container skinny-contain hero">
-    <h1 class="text-center mb-xl">Hey 👋, I’m Brad.</h1>
+    <h1 class="text-center mb-xl">Hey <span class="wave-emoji">👋</span>, I’m Brad.</h1>
     <div class="subtitle fs-3xl-regular">
-      I'm a <strong>Product Designer</strong> and <strong>Design Technologist</strong> experienced in building apps and design systems. I'm fluent in <strong>Figma</strong> & <strong>code</strong>. I love creating UIs that are elegant, impactful, and accessible.
+      I'm a&nbsp;<span class="gradient-highlight angle-one"><strong>&nbsp;Product Designer</strong> & <strong>Design Technologist </strong></span> experienced in building apps and design systems. I'm fluent in&nbsp;<span class="gradient-highlight angle-two"><strong>Figma</strong> & <strong>code</strong></span>. I love creating UIs that are elegant, impactful, and accessible.
     </div>
   </div>
   <portfolio />
