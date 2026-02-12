29de983fb15d6d830b7b306af5a5ff2b2b77b768
Brad Siefert <bradsiefert@icloud.com>
2026-02-12 16:07:58 -0600

    Updating GovOS Case Study to include product decisions and documentations

--- pages/case-studies/govos-design-system.vue
+++ pages/case-studies/govos-design-system.vue
@@ -13,11 +13,13 @@
             <h1>GovOS Design System</h1>
             <p>In 2021, I joined GovOS as the company's first in-house designer. They'd pieced design together for years through contractors and had no centralized visual design or design system. I was tasked to build a complete design system from scratch. I build foundations like color scale and typescale, 30+ components, and managed building the system in React.</p>
 
+            <p class="lead">I chose to work in govtech because I believe goverments should provide excellent services to its citizens. Building this system was an opportunity to improve how millions of people interact with essential public services.</p>
+
             <p class="mt-xl">
               <a href="https://kofile.github.io/design-system/?path=/docs/getting-started-installation--docs" rel="nofollow" target="_new" class="btn btn-primary btn-lg btn-govos-primary mb-xs">Storybook Documentation ↗</a>
               <a href="https://www.npmjs.com/package/@kofile/gds-foundations" rel="nofollow" target="_new" class="btn btn-primary btn-lg btn-govos-neutral mb-xs">Design Tokens @ NPM ↗</a>
             </p>
-            <hr>
+            <hr class="mb-3xl">
           </div>
         </div>
       </div>
@@ -41,13 +43,15 @@
       <div class="row"> 
         <div class="col-lg-12">
           <h2 class="mt-3xl">Problem & Needs</h2>
-          <p>GovOS delivers critical government services like records management, public records search, state and county tax payment, and short-term rental tax payment to hundreds of jurisdictions. It's products lacked any visual consistency, shared zero components or code, and federal/state web accessibility requirements had been ignored for many years. In my time at GovOS, I changed all of these things. <strong>During my tenure, I created a scalable design system that became the foundation for GovOS products and beyond.</strong></p>
+          <p>GovOS delivers critical government services like tax payments, business licensing, records management, public records search, short-term rental payments, and more to hundreds of jurisdictions. Before I joined, it's products lacked any visual consistency, shared zero components or code, and federal/state web accessibility requirements had been ignored for years. In my time at GovOS, I changed all of these things. <strong>During my tenure, I created a scalable design system that became the foundation for GovOS products and beyond.</strong></p>
           
-          <p>The lack of a cohesive visual design and a design system created significant inefficiencies for both designers and engineers. There weren't any shared UI components or platform level code. Every new product or feature was effectively starting from scratch, which led to duplicated effort and inconsistent patterns. Designers had no shared library to work from, so visual styles varied widely between applications. Because front-ends were so brittle, developers rarely made visual updates. They didn't have any design assets to go off so when they made changes they hard-coded colors, spacing, and typography and never addressed the overall design debt resulting in fragmented user interfaces.</p>
+          <p>The lack of a cohesive visual design and a design system created significant inefficiencies for both designers and engineers. There weren't any shared UI components or platform level code. Every new product or feature was effectively starting from scratch, which led to duplicated effort and inconsistent patterns. Designers had no shared library to work from, so visual styles varied widely between applications.</p> 
+          
+          <p>Because front-ends were so brittle, developers rarely made updates. They didn't have design assets to go off so when they made changes they hard-coded colors, spacing, and typography and never addressed the design debt, resulting in fragmented user interfaces.</p>
           
           <figure>
             <span class="ultrawide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-legacy-applications.webp" class="lightbox-trigger">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-legacy-applications.webp">
                 <img src="/portfolio/case-study-govos-legacy-applications.webp" alt="Screenshots of various GovOS products before the design system I created">
               </a>
             </span>
@@ -68,49 +72,124 @@
             <li class="mb-lg"><strong>Clean borders and almost no shadows.</strong> Instead of trendy shadows or heavy visual depth, I leaned into a very flat aesthetic. Cards and surfaces are defined with a light border (<code>color-neutral-300</code> in light mode) and were always set on <code>color-neutral-100</code> to have a subtle but clear contrast from the background. I wanted our UI to feel like a sheet of paper on a desk. This created a clean separation without any visual noise.</li>
             <li class="mb-2xl"><strong>High contrast and accessibile.</strong> GovOS's previous used overly subtle color opacities for their color palette. At times, they were almost indistinguishable from each other causing usability and accessibility issues. Accessibility had not been taken seriously in the past, but for me, it was non-negotiable. Rather than barely meeting minimum contrast requirements for text, I setup the system to well exceed them. Subtle text styles, tints, or shades were avoided. Our users needed clear differences in states, hierarchy, and affordances, so everything was designed for maximum clarity.</li>
           </ul>
-          <p>
+          <figure>
             <span class="wide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-color-palette.webp" class="lightbox-trigger">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-color-palette.webp">
                 <img src="/portfolio/case-study-govos-color-palette.webp" alt="">
               </a>
             </span>
-          </p>
-          <p>
+            <figcaption class="mt-0">GovOS's color scale and color roles</figcaption>
+          </figure>
+          <figure>
             <span class="wide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-typography-fontfamily.webp" class="lightbox-trigger">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-typography-fontfamily.webp">
                 <img src="/portfolio/case-study-govos-typography-fontfamily.webp" alt="">
               </a>
             </span>
-          </p>
-          <p>
+            <figcaption class="mt-0">GovOS's typography font family, Source Sans 3 in three weights, 480, 600, and 720</figcaption>
+          </figure>
+          <figure>
             <span class="wide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-typography-fontweight.webp" class="lightbox-trigger">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-typography-fontweight.webp">
                 <img src="/portfolio/case-study-govos-typography-fontweight.webp" alt="">
               </a>
             </span>
-          </p>
-          <p>
+            <figcaption class="mt-0">GovOS's typography scale, 12 steps from 3xs to 6xl</figcaption>
+          </figure>
+          <figure>
             <span class="wide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-colors-inverted.webp" class="lightbox-trigger">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-colors-inverted.webp">
                 <img src="/portfolio/case-study-govos-colors-inverted.webp" alt="">
               </a>
             </span>
-          </p>
-          <p>
+            <figcaption class="mt-0">GovOS's colors inverted</figcaption>
+          </figure>
+          <figure>
             <span class="wide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-colors-darkmode.webp" class="lightbox-trigger">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-colors-darkmode.webp">
                 <img src="/portfolio/case-study-govos-colors-darkmode.webp" alt="">
               </a>
             </span>
-          </p>
+            <figcaption class="mt-0">GovOS's components contrasted in light and dark mode</figcaption>
+          </figure>
           <hr>
 
-          <!-- <h2>Documenting for Design & Development</h2>
-          <p>I spend a huge amount of timing writing and creating design documentation for the system. I want the system to be incredibly easy to understand for both designers and developers. I think documentation is the differentiator between a system that is used and a system that is ignored. I occasionally contributed to the development documentation, but I wrote and created all of the design documentation and assets in Figma.</p>
+          <h2>Platform 🤝 Product</h2>
+          <p>Building a great design system has to include decisions that shape and serve products. It can't just be a toolkit of UI elements. Every component and pattern I create is meant to serve and align products and the business as a whole. <strong>The system is built to serve design, engineering, and product.</strong></p>
             
-          <p>Content writing seems like such a straightforward task, but anyone who has tried it knows will tell you it's actually incredibly difficult. It requires a deep understanding of design principles and engineering best practices and the ability to write in a way that is both precise and concise. It also requires a lot of time and effort to get right.</p>
-          <hr> -->
+          <p>Product alignment is extremely hard to accomplish. It requires auditing, alignment, vision, and buy-in. It's rare that product folks have the time to do this kind of work. They are often required to take a very myopic view that only includes their product. I think design systems are powerful tools for aligning platform-level patterns and my favorite example from my time at GovOS is global navigation.</p>
+          <figure>
+            <span class="ultrawide-img">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-product-navigation-annotations.webp">
+                <img src="/portfolio/case-study-govos-product-navigation-annotations.webp" alt="Screenshot of a global navigation with annotations labeling important sections like platform nativation and application title.">
+              </a>
+            </span>
+            <figcaption class="mt-0">Aligning and documenting the global navigation component with appropriate sections for future product needs.</figcaption>
+          </figure>
+          <figure>
+            <span class="ultrawide-img">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-product-navigation-sidenav.webp">
+                <img src="/portfolio/case-study-govos-product-navigation-sidenav.webp" alt="Screenshot of a global navigation header and sidebar navigation with an example flyover panel with links.">
+              </a>
+            </span>
+            <figcaption class="mt-0">Building the sidebar navigation component was vital because half our products needed multiple levels of navigation.</figcaption>
+          </figure>
+          <figure>
+            <span class="ultrawide-img">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-product-navigation-colormodes.webp">
+                <img src="/portfolio/case-study-govos-product-navigation-colormodes.webp" alt="Screenshot of two global navigation headers, one in light mode, one in dark mode.">
+              </a>
+            </span>
+            <figcaption class="mt-0">I build the global navigation component in light and dark mode to align with future product goals of accessibility and theming.</figcaption>
+          </figure>
+          <figure>
+            <span class="ultrawide-img">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-product-navigation-mobile.webp">
+                <img src="/portfolio/case-study-govos-product-navigation-mobile.webp" alt="Screenshot of how our global navigation component works on mobile screens.">
+              </a>
+            </span>
+            <figcaption class="mt-0">A major issue product teams wanted was a unified mobile navigation for global navigation. I design this to scale to all our products.</figcaption>
+          </figure>
+          <figure>
+            <span class="ultrawide-img">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-product-navigation-beforeafter.webp">
+                <img src="/portfolio/case-study-govos-product-navigation-beforeafter.webp" alt="Screenshot of a global navigation before and after examples of headers pre and post-design system. The post-design system ones look better. 😉">
+              </a>
+            </span>
+            <figcaption class="mt-0">Pre-design system headers vs design system global navigation components build for each product</figcaption>
+          </figure>
+          <hr>
 
+          <h2>Documentation for Everyone</h2>
+          <p>Documentation is the differentiator between a system that is used and a system that is ignored. I spend a huge amount of time writing and creating design documentation for the system. Above all, I want the system to be easy to use and understand for both designers and developers. Writing plain written and clear documentation and creating assets to explain the documentation, makes that a reality.</p>
+            
+          <p>Content writing seems like such a straightforward task, but anyone who has tried it will tell you it's incredibly difficult. It requires a deep understanding of design principles and engineering best practices and the ability to write in a way that is both precise and concise. I never considered a component or pattern done until the design and development documentation was written. Without great docs, there's no chance it'll be used right.</p>
+          <figure>
+            <span class="wide-img">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-docs-dialog.webp">
+                <img src="/portfolio/case-study-govos-docs-dialog.webp" alt="">
+              </a>
+            </span>
+            <figcaption class="mt-0">Documentation of the Dialog component. Every component has docs written for designers, developers, PMs, and LLMs.</figcaption>
+          </figure>
+          <figure>
+            <span class="wide-img">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-docs-emptystate.webp">
+                <img src="/portfolio/case-study-govos-docs-emptystate.webp" alt="">
+              </a>
+            </span>
+            <figcaption class="mt-0">Documentation isn't just words, it's user flows, interaction states, and usages guidance.</figcaption>
+          </figure>
+          <figure>
+            <span class="wide-img">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-docs-menu.webp">
+                <img src="/portfolio/case-study-govos-docs-menu.webp" alt="">
+              </a>
+            </span>
+            <figcaption class="mt-0">When creating docs I highlight variants to designers and developers to make sure they are aware of all component options.</figcaption>
+          </figure>
+          <hr>
+          
           <h2>In Practice</h2>
           <p>Once the system foundations were solid and core components were created, the next step was collaborating with other UX designers to create mockups and user flows. Designers used the system to create a redesigned tax portal for State of Colorado and Alaska, updated our public search portal, and created platform components to be shared across all applications.</p>
           
@@ -118,24 +197,24 @@
           
           <figure>
             <span class="ultrawide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-apps-cdor.webp" class="lightbox-trigger">
-                <img src="/portfolio/case-study-govos-apps-cdor.webp" alt="">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-apps-cdor.webp">
+                <img src="/portfolio/case-study-govos-apps-cdor.webp" alt="Screenshot of two pages in a state tax portal web application.">
               </a>
             </span>
             <figcaption class="mt-0">GovOS's Tax and Licensing product for states and counties built with the new design system</figcaption>
           </figure>
           <figure>
             <span class="ultrawide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-apps-search.webp" class="lightbox-trigger">
-                <img src="/portfolio/case-study-govos-apps-search.webp" alt="">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-apps-search.webp">
+                <img src="/portfolio/case-study-govos-apps-search.webp" alt="Screenshot of two pages in a official records search web application.">
               </a>
             </span>
             <figcaption class="mt-0">GovOS's Record Search product using the new design system</figcaption>
           </figure>
           <figure>
             <span class="ultrawide-img">
-              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-apps-platform.webp" class="lightbox-trigger">
-                <img src="/portfolio/case-study-govos-apps-platform.webp" alt="">
+              <a target="_blank" rel="nofollow" href="/portfolio/case-study-govos-apps-platform.webp">
+                <img src="/portfolio/case-study-govos-apps-platform.webp" alt="Screenshot of two pages in platform components in a web application.">
               </a>
             </span>
             <figcaption class="mt-0">Example of GovOS platform components</figcaption>
